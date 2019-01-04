8e9e746a8564f652be5bc81870ec914950a71d38
Bart Vandeputte <bart.vdputte@gmail.com>
2019-01-04 11:02:29 +0100

    DEV: Fix typo

--- snippets/gtm-noscript.php
+++ snippets/gtm-noscript.php
@@ -1,4 +1,4 @@
-<?php if (c::get('gtm.propId') !== ""): ?>
+<?php if (c::get('gtm.propId') != ""): ?>
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= c::get('gtm.propId'); ?>"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <?php endif; ?>
--- snippets/gtm.php
+++ snippets/gtm.php
@@ -1,4 +1,4 @@
-<?php if (c::get('gtm.propId') !== ""): ?>
+<?php if (c::get('gtm.propId') != ""): ?>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
