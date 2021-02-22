a8fdbc20ee74be6254325569b91448996a7ad1e0
Bart Vandeputte <mail@bartvandeputte.be>
2021-02-22 17:36:32 +0100

    Fix option names

--- snippets/gtm-noscript.php
+++ snippets/gtm-noscript.php
@@ -1,4 +1,4 @@
 <?php if (option("bvdputte.gtm.propId") !== ""): ?>
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= option('bvdputte.ga.propId') ?>"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= option('bvdputte.gtm.propId') ?>"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <?php endif ?>
